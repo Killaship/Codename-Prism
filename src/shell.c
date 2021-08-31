d293b7ecc60f62abf5b934ffbe2fc3c8a496a71b
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-31 18:30:01 -0400

    Create shell.c

---
+++ src/shell.c
@@ -0,0 +1,29 @@
+#include <stdio.h> // when/if ported to freestanding OS, replace with appropriate includes.
+
+#define EXIT_SUCCESS 0;
+#define EXIT_FAIL 1;
+int error = 0;
+int running;
+
+void shell_loop(void) {
+  
+  while(running) {
+    // do stuff
+  }
+  
+}
+
+int main(void) {
+  // load any possible config files (added in future)
+
+  running = 1;
+  shell_loop();
+
+  // perform any shutdown/cleanup (also added in future)
+  if(!error) {
+    return EXIT_SUCCESS;
+  }
+  else {
+    return EXIT_FAIL;
+  
+}
