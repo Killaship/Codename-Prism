a11065cc183ffce887b73a23542dd031042fce56
Killaship <69988679+Killaship@users.noreply.github.com>
2021-09-24 07:05:38 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,4 @@ echo "Now building..."
 git pull
 gcc src/shell.c -Wall -Wextra -o prism
 echo "Building complete, now running."
-echo ""
-echo "TIP FOR NOOBZ:"
-echo "If program hangs, press ^C to quit. (Assuming you're running under linux, with bash shell.)"
-echo ""
 ./prism
