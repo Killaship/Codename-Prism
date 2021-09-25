911a066f3c5d12fe812ff85c4138283346951780
Killaship <69988679+Killaship@users.noreply.github.com>
2021-09-25 09:41:09 -0400

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,6 @@
 // todo: everything
 // https://theartofmachinery.com/2018/11/07/writing_a_nix_shell.html
-
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,15 +15,15 @@ void main() {
   }
 }
 
-void runcmd(char cmd) {
+int runcmd(char cmd) {
   int pid = fork();
   if(pid < 0) { // error for some reason
     printf("Can't create a new process!");
     exit(1);
   }
   if(pid == 0) { // child process
-    char* arr[] = {str, NULL};
-    execv(str, arr);
+    char* arr[] = {cmd, NULL};
+    execv(cmd, arr);
     
     printf("Command execution failed!"); // you should only get here if execv() failed
     exit(1);
@@ -32,5 +32,5 @@ void runcmd(char cmd) {
   wait(&status); // just wait until child is done
   // This is the exit code of the child
   // (Conventially zero means okay, non-zero means error)
-  return WEXITSTATUS(status);
+  return status;
 }
