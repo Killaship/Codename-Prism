d6647dde71e22e10cb0e24a90bb55b0cd72f16fd
Killaship <69988679+Killaship@users.noreply.github.com>
2021-09-25 09:38:23 -0400

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -3,6 +3,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 void main() {
   const char *prompt = ">";
@@ -10,7 +11,26 @@ void main() {
   while(1) {
     printf(prompt);
     scanf("%s", str);
+    runcmd(str);
+  }
+}
+
+void runcmd(char cmd) {
+  int pid = fork();
+  if(pid < 0) { // error for some reason
+    printf("Can't create a new process!");
+    exit(1);
+  }
+  if(pid == 0) { // child process
     char* arr[] = {str, NULL};
     execv(str, arr);
+    
+    printf("Command execution failed!"); // you should only get here if execv() failed
+    exit(1);
   }
+  int status;
+  wait(&status); // just wait until child is done
+  // This is the exit code of the child
+  // (Conventially zero means okay, non-zero means error)
+  return WEXITSTATUS(status);
 }
